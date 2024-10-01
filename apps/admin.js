00a62863b73b2bfd9094a0710246b78a52f162d4
qsyhh <2122840028@qq.com>
2024-10-01 16:25:13 +0800

    ⚡️feat: #喵喵背景帮助

--- apps/admin.js
+++ apps/admin.js
@@ -41,6 +41,11 @@ app.reg({
     fn: sysCfg,
     desc: "【#管理】系统设置"
   },
+  bgHelp: {
+    rule: /^#?喵喵背景帮助$/,
+    fn: bgHelp,
+    desc: "【#管理】背景设置帮助"
+  },
   miaoApiInfo: {
     rule: /^#喵喵api$/,
     fn: miaoApiInfo,
@@ -77,14 +82,18 @@ async function sysCfg(e) {
     let val = regRet[2] || ""
 
     let cfgSchema = cfgSchemaMap[regRet[1]]
-    if (cfgSchema.input) {
-      val = cfgSchema.input(val)
-    } else if (cfgSchema.type === "str") {
-      val = (val || cfgSchema.def) + ""
-    } else {
-      val = cfgSchema.type === "num" ? (val * 1 || cfgSchema.def) : !/关闭/.test(val)
-    }
+    val = valDeal(val, cfgSchema)
     Cfg.set(cfgSchema.cfgKey, val, isBackground)
+  } else if ((isBackground && /^全部/.exec(regRet[2]))) {
+    let type = "str"
+    if (/^全部模糊/.exec(regRet[2])) type = "num"
+    lodash.forEach(Object.keys(cfgSchemaMap), (cm) => {
+      let val = regRet[2].replace(/^全部(模糊)?/, "") || ""
+      if (cfgSchemaMap[cm].type == type) {
+        val = valDeal(val, cfgSchemaMap[cm])
+        Cfg.set(cfgSchemaMap[cm].cfgKey, val, isBackground)
+      }
+    })
   }
 
   let schema = Cfg.getCfgSchema(isBackground)
@@ -143,6 +152,23 @@ async function updateMiaoPlugin(e) {
   return up.update()
 }
 
+async function bgHelp(e) {
+  return e.reply(
+    "【#喵喵背景设置帮助】\n" +
+    "#喵喵背景设置列表xxx --- 设置面板列表背景图\n" +
+    "#喵喵背景设置面板xxx --- 设置xx面板背景图\n" +
+    "#喵喵背景设置列表模糊[0-50] --- 设置面板列表背景图模糊度\n" +
+    "#喵喵背景设置面板模糊[0-50] --- 设置面板背景图模糊度\n" +
+    "-------------------\n" +
+    "#喵喵背景设置全部xxx --- 一键设置面板、面板列表背景图\n" +
+    "#喵喵背景设置全部模糊[0-50] --- 一键设置面板、面板列表模糊度\n" +
+    "-------------------\n" +
+    "上述xxx替换为绝对路径/相对路径/图链，为def时为关闭自定义背景图\n" +
+    `插件绝对路径：${e.isMaster && e.isPrivate ? miaoPath : e.isMaster ? "请私聊查看" : "null"}\n` +
+    "插件相对路径：../../../../../plugins/miao-plugin/", true
+  )
+}
+
 async function Miaoupdatelog(e, plugin = "miao-plugin") {
   let cm = "git log  -20 --oneline --pretty=format:\"%h||[%cd]  %s\" --date=format:\"%F %T\""
   if (plugin) cm = `cd ./plugins/${plugin}/ && ${cm}`
@@ -184,4 +210,15 @@ async function miaoApiInfo(e) {
   let data = await req.json()
   if (data.status !== 0) return e.reply("token检查错误，请求失败")
   e.reply(data.msg)
+}
+
+function valDeal(val, cfgSchema) {
+  if (cfgSchema.input) {
+    val = cfgSchema.input(val)
+  } else if (cfgSchema.type === "str") {
+    val = (val || cfgSchema.def) + ""
+  } else {
+    val = cfgSchema.type === "num" ? (val * 1 || cfgSchema.def) : !/关闭/.test(val)
+  }
+  return val
 }
--- config/system/background_system.js
+++ config/system/background_system.js
@@ -6,8 +6,8 @@ export const cfgProfile = {
         title: "面板列表背景图模糊度",
         key: "列表模糊",
         type: "num",
-        def: 10,
-        input: (n) => Math.min(50, Math.max(0, (n * 1 || 10))),
+        def: 7,
+        input: (n) => Math.min(50, Math.max(0, (n * 1 || 7))),
         desc: "#面板列表背景图模糊度，可选值0~50"
       },
       background_list: {
@@ -15,14 +15,14 @@ export const cfgProfile = {
         key: "列表",
         type: "str",
         def: "def",
-        desc: "#面板列表背景图，可键入绝对路径、相对路径(插件目录相对路径：../../../../../plugins/miao-plugin/)、api图链，使用#喵喵背景设置列表def 以关闭列表背景图"
+        desc: "#面板列表背景图，使用#喵喵背景帮助查看帮助"
       },
       filter_profile: {
         title: "面板背景图模糊度",
         key: "面板模糊",
         type: "num",
-        def: 10,
-        input: (n) => Math.min(50, Math.max(0, (n * 1 || 10))),
+        def: 7,
+        input: (n) => Math.min(50, Math.max(0, (n * 1 || 7))),
         desc: "#xx面板背景图模糊度，可选值0~50"
       },
       background_profile: {
@@ -30,7 +30,7 @@ export const cfgProfile = {
         key: "面板",
         type: "str",
         def: "def",
-        desc: "#xx面板背景图，可键入绝对路径、相对路径(插件目录相对路径：../../../../../plugins/miao-plugin/)、api图链，使用#喵喵背景设置面板def 以关闭列表背景图"
+        desc: "#xx面板背景图，使用#喵喵背景帮助查看帮助"
       }
     }
   }
