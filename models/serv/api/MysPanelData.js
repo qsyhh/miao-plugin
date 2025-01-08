070c044ba1cf7ad5c8e8376a7c5b97cad0c8dfff
qsyhh <2122840028@qq.com>
2025-01-09 00:25:04 +0800

    🐛fix: 米游社面板天赋等级错误

--- models/serv/api/MysPanelData.js
+++ models/serv/api/MysPanelData.js
@@ -6,18 +6,21 @@ import { artifactMainIdMapping, propertyType2attrName, fixedAttrNames } from "./
 
 let MysPanelData = {
   setAvatar(player, ds) {
-    let char = Character.get(ds.base.id)
-    let avatar = player.getAvatar(ds.base.id, true)
+    let { id, element, level, fetter, actived_constellation_num } = ds.base
+    let elem = Format.elem(element)
+    logger.info({ id, elem })
+    let char = Character.get({ id, elem })
+    let avatar = player.getAvatar(id, true)
     if (!char) return false
 
     avatar.setAvatar({
-      level: ds.base.level,
-      cons: ds.base.actived_constellation_num,
-      fetter: ds.base.fetter,
+      level,
+      elem,
+      fetter,
+      cons: actived_constellation_num,
       costume: ds.costumes?.[0]?.id || 0,
-      elem: Format.elem(ds.base.element),
       weapon: MysPanelData.getWeapon(ds.weapon),
-      talent: MysPanelData.getTalent(char, ds.skills),
+      talent: MysPanelData.getTalent(char, actived_constellation_num, ds.skills),
       artis: MysPanelData.getArtifact(ds.relics)
     }, "mysPanel")
     return avatar
@@ -33,17 +36,22 @@ let MysPanelData = {
     }
   },
 
-  getTalent(char, ds = []) {
-    let { talentId = {} } = char.meta
+  getTalent(char, cons, ds = []) {
+    let { talentId = {}, talentCons = {} } = char.meta
+    let idx = 0
     let ret = {}
     lodash.forEach(ds, (talent_data) => {
-      if (talent_data.skill_type === 1) {
-        let key
-        if (talentId[talent_data.skill_id]) {
-          key = talentId[talent_data.skill_id]
-          ret[key] = talent_data.level
-        }
+      let key
+      if (talentId[talent_data.skill_id]) {
+        let key = talentId[talent_data.skill_id]
+        ret[key] = talent_data.level
+      } else if (talent_data.skill_type === 1) {
+        key = [ "a", "e", "q" ][idx++]
+        ret[key] = ret[key] || talent_data.level
       }
+    })
+    lodash.forEach(talentCons, (lv, key) => {
+      if (ret[key] && cons >= lv) ret[key] = Math.max(1, ret[key] - 3)
     })
     return ret
   },
