c815227392f18e97839523c21e0fdd681704bf1a
其实雨很好(嘿弩版) <2122840028@qq.com>
2025-02-01 00:07:10 +0800

    ♻️res(meta-gs): 玛拉妮伤害计算重置

--- resources/meta-gs/character/玛拉妮/calc.js
+++ resources/meta-gs/character/玛拉妮/calc.js
@@ -1,52 +1,55 @@
 export const details = [
   {
-    title: "巨浪鲨鲨撕咬伤害",
-    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * (talent.e["鲨鲨撕咬基础伤害"] + talent.e["巨浪鲨鲨撕咬伤害额外提升"]) / 100, "a,nightsoul")
+    title: "E后鲨鲨撕咬基础伤害",
+    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.e["鲨鲨撕咬基础伤害"] / 100, "a,nightsoul")
   }, {
-    check: ({ cons }) => cons < 6,
-    title: "巨浪鲨鲨撕咬蒸发",
-    dmgKey: "e",
-    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * (talent.e["鲨鲨撕咬基础伤害"] + talent.e["巨浪鲨鲨撕咬伤害额外提升"]) / 100, "a,nightsoul", "vaporize")
+    title: "E后鲨鲨撕咬一层伤害",
+    params: { buffCount: 1 },
+    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.e["鲨鲨撕咬基础伤害"] / 100, "a,nightsoul")
   }, {
-    check: ({ cons }) => cons >= 1,
-    title: "强化巨浪鲨鲨撕咬蒸发",
-    dmgKey: "e",
-    params: { cons1: true },
-    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * (talent.e["鲨鲨撕咬基础伤害"] + talent.e["巨浪鲨鲨撕咬伤害额外提升"]) / 100, "a,nightsoul", "vaporize")
+    title: "E后鲨鲨撕咬二层伤害",
+    params: { buffCount: 2 },
+    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.e["鲨鲨撕咬基础伤害"] / 100, "a,nightsoul")
   }, {
-    title: "爆瀑飞弹伤害",
-    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.q["技能伤害"] / 100, "q,nightsoul")
+    title: "E后巨浪鲨鲨撕咬伤害",
+    params: { buffCount: 3 },
+    dmg: ({ talent, calc, attr, cons }, { basic }) => basic(calc(attr.hp) * (talent.e["鲨鲨撕咬基础伤害"] + (cons > 0 ? 66 : 0)) / 100, "a,nightsoul")
   }, {
-    title: "爆瀑飞弹蒸发",
-    dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.q["技能伤害"] / 100, "q,nightsoul", "vaporize")
+    title: "E后巨浪鲨鲨撕咬蒸发",
+    params: { buffCount: 3 },
+    dmg: ({ talent, calc, attr, cons }, { basic }) => basic(calc(attr.hp) * (talent.e["鲨鲨撕咬基础伤害"] + (cons > 0 ? 66 : 0)) / 100, "a,nightsoul", "vaporize")
+  }, {
+    title: "Q爆瀑飞弹伤害",
+    dmg: ({ talent, calc, attr, cons }, { basic }) => basic(calc(attr.hp) * (talent.q["技能伤害"] / 100 + (cons > 0 ? 66 : 0)), "q,nightsoul")
+  }, {
+    title: "Q爆瀑飞弹蒸发",
+    dmg: ({ talent, calc, attr, cons }, { basic }) => basic(calc(attr.hp) * (talent.q["技能伤害"] / 100 + (cons > 0 ? 66 : 0)), "q,nightsoul", "vaporize")
   }
 ]
 
+export const defDmgIdx = 4
 export const defParams = { Nightsoul: true }
-export const defDmgKey = "e"
 export const mainAttr = "hp,cpct,cdmg,mastery"
 
 export const buffs = [
   {
-    title: "踏鲨破浪：3层浪势充能使鲨鲨撕咬造成的伤害提升[aPlus]巨浪鲨鲨撕咬伤害额外提升[_aPlus]",
+    title: "元素战技-踏鲨破浪：对敌人施加猎物标记，每层浪势充能使造成的伤害提高[_aPlus1]，拥有3层浪势充能时，造成的伤害进一步提升[_aPlus2]",
     sort: 9,
     data: {
-      aPlus: ({ talent, calc, attr }) => calc(attr.hp) * talent.e["浪势充能伤害提升"] / 100 * 3,
-      _aPlus: ({ talent, calc, attr }) => calc(attr.hp) * talent.e["巨浪鲨鲨撕咬伤害额外提升"] / 100
+      aPlus: ({ talent, calc, attr, params }) => {
+        if (!params.buffCount) return false
+        let cost = params.buffCount === 3 ? talent.e["巨浪鲨鲨撕咬伤害额外提升"] : 0
+        return calc(attr.hp) * (talent.e["浪势充能伤害提升"] * params.buffCount + cost) / 100
+      },
+      _aPlus1: ({ talent, calc, attr }) => calc(attr.hp) * talent.e["浪势充能伤害提升"] / 100,
+      _aPlus2: ({ talent, calc, attr }) => calc(attr.hp) * talent.e["巨浪鲨鲨撕咬伤害额外提升"] / 100
     }
   }, {
-    title: "玛拉妮天赋：3层逐浪心得使爆瀑飞弹伤害提升[qPlus]",
-    sort: 9,
-    data: {
-      qPlus: ({ calc, attr }) => calc(attr.hp) * 45 / 100
-    }
-  }, {
-    check: ({ params }) => params.cons1 === true,
-    title: "玛拉妮1命：第一次巨浪鲨鲨撕咬及它所触发的鲨鲨飞弹造成的伤害提升[aPlus]",
+    title: "玛拉妮1命：进入夜魂加持状态后的第一次巨浪鲨鲨撕咬及它所触发的鲨鲨飞弹造成的伤害提升[_aPlus]",
     cons: 1,
     sort: 9,
     data: {
-      aPlus: ({ calc, attr }) => calc(attr.hp) * 66 / 100
+      _aPlus: ({ calc, attr }) => calc(attr.hp) * 66 / 100
     }
   }, {
     title: "玛拉妮4命：爆瀑飞弹造成的伤害提升[qDmg]%",
@@ -55,16 +58,6 @@ export const buffs = [
       qDmg: 75
     }
   }
-  /*
-  , {
-    title: '幻境祝福：角色编入队伍后，生命值、攻击力与防御力提升[hpPct]%,该效果不论是否在幻想真境剧诗中都将生效。',
-    data: {
-      hpPct: 20,
-      atkPct: 20,
-      defPct: 20
-    }
-  }
-  */
 ]
 
-export const createdBy = "liangshi"
+export const createdBy = "其实雨很好"
