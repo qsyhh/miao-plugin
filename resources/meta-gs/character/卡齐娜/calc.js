02a06f969f3b27cd8f8ece54044cc0d5af12f7f6
其实雨很好(嘿弩版) <2122840028@qq.com>
2025-02-02 03:43:28 +0800

    ♻️res(meta-gs): 艾梅莉埃、卡齐娜计算重置

--- resources/meta-gs/character/卡齐娜/calc.js
+++ resources/meta-gs/character/卡齐娜/calc.js
@@ -1,38 +1,39 @@
 export const details = [
   {
-    title: "冲天转转主动伤害",
+    title: "E冲天转转搭乘伤害",
     dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.def) * talent.e["冲天转转搭乘伤害"] / 100, "e,nightsoul")
   }, {
-    title: "冲天转转自动伤害",
+    title: "E冲天转转独立伤害",
     dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.def) * talent.e["冲天转转独立伤害"] / 100, "e,nightsoul")
   }, {
-    title: "Q释放伤害",
+    title: "Q技能伤害",
     dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.def) * talent.q["技能伤害"] / 100, "q")
   }, {
-    check: ({ cons }) => cons >= 6,
-    title: "护盾替换摧毁伤害",
+    check: ({ cons }) => cons === 6,
+    title: "6命护盾摧毁伤害",
     dmg: ({ calc, attr }, { basic }) => basic(calc(attr.def) * 200 / 100, "")
   }
 ]
 
+export const defDmgIdx = 2
 export const defParams = { Nightsoul: true }
-export const mainAttr = "def,cpct,cdmg,mastery"
+export const mainAttr = "def,cpct,cdmg"
 
 export const buffs = [
   {
-    title: "卡齐娜天赋：附近的角色触发「夜魂迸发」,岩元素伤害提升[dmg]%。",
+    title: "天赋-山的回声：队伍中的附近的角色触发「夜魂迸发」后，卡齐娜的岩元素伤害加成提升[dmg]%(单人不触发)",
     data: {
       dmg: 20
     }
   }, {
-    title: "卡齐娜天赋：冲天转转造成的伤害提升[ePlus]%。",
+    title: "天赋-坚岩之重：冲天转转造成的伤害提升[ePlus]%。",
     sort: 9,
     data: {
       ePlus: ({ calc, attr }) => calc(attr.def) * 20 / 100,
       qPlus: ({ calc, attr }) => calc(attr.def) * 20 / 100
     }
   }, {
-    title: "卡齐娜4命：领域中的队伍中当前场上角色的防御力提升[defPct]%",
+    title: "卡齐娜4命：存在4名敌人，领域中的队伍中当前场上角色的防御力提升[defPct]%",
     cons: 4,
     data: {
       defPct: 20
@@ -40,4 +41,4 @@ export const buffs = [
   }
 ]
 
-export const createdBy = "liangshi"
+export const createdBy = "其实雨很好"
--- resources/meta-gs/character/艾梅莉埃/calc.js
+++ resources/meta-gs/character/艾梅莉埃/calc.js
@@ -1,28 +1,31 @@
 /* eslint-disable no-empty-pattern */
 export const details = [
   {
-    title: "E释放伤害",
+    title: "重击伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.a["重击伤害"], "a2")
+  }, {
+    title: "E技能伤害",
     params: { e: true },
     dmg: ({ talent }, dmg) => dmg(talent.e["技能伤害"], "e")
   }, {
-    title: "柔灯之匣一阶伤害",
+    title: "E后柔灯之匣一阶伤害",
     params: { e: true },
     dmg: ({ talent }, dmg) => dmg(talent.e["柔灯之匣·一阶攻击伤害"], "e")
   }, {
-    title: "柔灯之匣二阶单枚伤害",
+    title: "E后柔灯之匣二阶单枚伤害",
     params: { e: true },
     dmg: ({ talent }, dmg) => dmg(talent.e["柔灯之匣·二阶攻击伤害2"][0], "e")
   }, {
-    title: "后台柔灯之匣二阶单枚",
     check: ({ weapon }) => weapon.name === "息灾",
+    title: "后台柔灯之匣二阶单枚",
     params: { e: true, off_field: true },
     dmg: ({ talent }, dmg) => dmg(talent.e["柔灯之匣·二阶攻击伤害2"][0], "e")
   }, {
-    title: "天赋浸析伤害",
+    title: "天赋清露香氛伤害",
     params: { e: true },
     dmg: ({ attr }, { basic }) => basic(attr.atk * 600 / 100, "")
   }, {
-    title: "柔灯之匣三阶(Q)伤害",
+    title: "Q柔灯之匣三阶伤害",
     dmg: ({ talent }, dmg) => dmg(talent.q["柔灯之匣·三阶攻击伤害"], "q")
   }, {
     title: "Q完整对单",
@@ -34,10 +37,6 @@ export const details = [
         avg: q1.avg * cons4
       }
     }
-  }, {
-    check: ({ cons }) => cons >= 6,
-    title: "Q后重击伤害",
-    dmg: ({ talent }, dmg) => dmg(talent.a["重击伤害"], "a2")
   }, {
     title: "燃烧反应伤害",
     dmg: ({}, { reaction }) => reaction("burning")
@@ -54,20 +53,19 @@ export const buffs = [
       dmg: ({ attr }) => Math.min(36, attr.atk / 1000 * 15)
     }
   }, {
-    check: ({ params }) => params.e === true,
-    title: "艾梅莉埃1命：[淡香浸析] e与天赋造成的伤害提升[dmg]%",
+    title: "艾梅莉埃1命：元素战技撷萃调香与天赋的清露香氛造成的伤害提升20%",
     cons: 1,
     data: {
-      dmg: 20
+      dmg: ({ params }) => params.e ? 20 : 0
     }
   }, {
-    title: "艾梅莉埃2命：EQ命中的敌人草元素抗性降低[kx]%",
+    title: "艾梅莉埃2命：攻击命中敌人时，该敌人的草元素抗性降低[kx]%",
     cons: 2,
     data: {
       kx: 30
     }
   }, {
-    title: "艾梅莉埃6命：施放EQ时,普攻与重击造成的伤害提升[aPlus]",
+    title: "艾梅莉埃6命：施放元素战技与元素爆发后,普攻与重击造成的伤害提升[aPlus]",
     cons: 6,
     data: {
       aPlus: ({ attr }) => attr.atk * 300 / 100,
@@ -76,4 +74,4 @@ export const buffs = [
   }
 ]
 
-export const createdBy = "liangshi"
+export const createdBy = "其实雨很好"
