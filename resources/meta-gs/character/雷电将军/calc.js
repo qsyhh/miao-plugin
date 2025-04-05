7603df8c1cc3b306bd8fde32ae0692b0dc3f5b7a
其实雨很好 <2122840028@qq.com>
2025-04-05 14:19:43 +0800

    ♻️refactor(meta-gs): 雷电将军计算重置

--- resources/meta-gs/character/雷电将军/calc.js
+++ resources/meta-gs/character/雷电将军/calc.js
@@ -1,58 +1,58 @@
 export const details = [
   {
-    title: "零愿力Q首刀",
-    params: {
-      type: 1,
-      num: 0
-    },
-    dmg: ({ talent, attr }, dmg) => dmg(talent.q["梦想一刀基础伤害"], "q")
+    title: "E技能伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["技能伤害"], "e")
   }, {
-    title: "满愿力Q首刀",
-    params: {
-      type: 0,
-      num: 60
-    },
-    dmg: ({ talent, attr }, dmg) => dmg(talent.q["梦想一刀基础伤害"], "q")
+    title: "E协同攻击伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["协同攻击伤害"], "e")
   }, {
-    title: "满愿力Q后重击",
-    params: {
-      type: 1,
-      num: 60
-    },
-    dmg: ({ talent, attr }, dmg) => dmg(talent.q["重击伤害"], "q")
+    title: "Q梦想一刀伤害(零愿力)",
+    dmg: ({ talent }, dmg) => dmg(talent.q["梦想一刀基础伤害"], "q")
+  }, {
+    title: "Q梦想一刀伤害(满愿力)",
+    params: { type: 0, num: 60 },
+    dmg: ({ talent }, dmg) => dmg(talent.q["梦想一刀基础伤害"], "q")
+  }, {
+    title: "Q后普攻一段伤害(满愿力)",
+    params: { type: 1, num: 60 },
+    dmg: ({ talent }, dmg) => dmg(talent.q["一段伤害"], "q")
+  }, {
+    title: "Q后重击伤害(满愿力)",
+    params: { type: 1, num: 60 },
+    dmg: ({ talent }, dmg) => dmg(talent.q["重击伤害"], "q")
   }
 ]
 
-export const defParams = {
-  num: 60,
-  type: 0
-}
-
-export const defDmgIdx = 1
+export const defDmgIdx = 3
 export const mainAttr = "atk,cpct,cdmg,recharge,dmg"
 
 export const buffs = [
   {
-    title: "恶曜开眼：开E元素爆发伤害提升[qDmg]%",
+    title: "元素战技-神变·恶曜开眼：元素战技期间元素爆发伤害提升[qDmg]%",
     data: {
       qDmg: ({ talent }) => talent.e["元素爆发伤害提高"] * 90
     }
   }, {
-    title: "梦想真说：Q满愿力获得[qPct]%大招倍率加成",
+    title: "元素爆发-梦想真说：每消耗一层诸愿百眼之轮的愿力，提升梦想一刀[_qDmg1]%的倍率，与梦想一心[_qDmg2]%的倍率",
     data: {
-      qPct: ({ talent, params }) => talent.q["愿力加成"][params.type || 0] * params.num
+      _qDmg1: ({ talent }) => talent.q["愿力加成"][0],
+      _qDmg2: ({ talent }) => talent.q["愿力加成"][1],
+      qPct: ({ talent, params }) => "type" in params ? talent.q["愿力加成"][params.type] * params.num : 0
     }
   }, {
-    check: ({ cons }) => cons >= 2,
-    title: "雷神2命：大招无视敌人[qIgnore]%防御力",
+    title: "天赋-殊胜之御体：[_recharge]%元素充能效率，获得[dmg]%雷伤加成",
+    sort: 4,
+    data: {
+      _recharge: ({ attr, calc }) => calc(attr.recharge),
+      dmg: ({ attr, calc }) => Math.max(calc(attr.recharge) - 100, 0) * 0.4
+    }
+  }, {
+    cons: 2,
+    title: "雷电将军2命：元素爆发造成的伤害无视敌人[qIgnore]%防御力",
     data: {
       qIgnore: 60
     }
-  }, {
-    title: "雷神被动：基于元素充能获得[dmg]%雷伤加成",
-    sort: 4,
-    data: {
-      dmg: ({ attr }) => Math.max(attr.recharge.base + attr.recharge.plus - 100, 0) * 0.4
-    }
   }
 ]
+
+export const createdBy = "其实雨很好"
