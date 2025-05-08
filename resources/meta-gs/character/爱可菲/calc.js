8fd4152d0ccb8333be59d7d83633bfff915c45a1
其实雨很好 <2122840028@qq.com>
2025-05-08 22:39:23 +0800

    ⚡️perf: 爱可菲、伊法伤害计算

--- resources/meta-gs/character/爱可菲/calc.js
+++ resources/meta-gs/character/爱可菲/calc.js
@@ -1,23 +1,28 @@
 export const details = [
   {
     title: "E技能伤害",
-    dmg: ({ talent, attr }, dmg) => dmg(talent.e["技能伤害"], "e")
+    dmg: ({ talent }, dmg) => dmg(talent.e["技能伤害"], "e")
   }, {
-    title: "E「冻霜芭菲」伤害",
-    dmg: ({ talent, attr }, dmg) => dmg(talent.e["冻霜芭菲伤害"], "e")
+    title: "E冻霜芭菲伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["冻霜芭菲伤害"], "e")
+  }, {
+    title: "流涌之刃伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["流涌之刃伤害"], "e")
   }, {
     title: "Q技能伤害",
-    dmg: ({ talent, attr }, dmg) => dmg(talent.q["技能伤害"], "q")
+    dmg: ({ talent }, dmg) => dmg(talent.q["技能伤害"], "q")
   }, {
-    title: "Q释放治疗量",
+    title: "Q治疗量",
     dmg: ({ talent, attr, calc }, { heal }) => heal(talent.q["治疗量2"][0] / 100 * calc(attr.atk) + talent.q["治疗量2"][1])
   }, {
     title: "天赋每跳治疗",
+    params: { kfsl: true },
     dmg: ({ attr, calc }, { heal }) => heal(138.24 / 100 * calc(attr.atk))
   }, {
-    title: "6命额外「特级冻霜芭菲」伤害",
-    cons: 6,
-    dmg: ({ attr, calc }, { basic }) => basic(calc(attr.atk) * 500 / 100, "e")
+    check: ({ cons }) => cons > 5,
+    title: "6命特级冻霜芭菲伤害",
+    // eslint-disable-next-line no-empty-pattern
+    dmg: ({}, dmg) => dmg(500, "e")
   }
 ]
 
@@ -26,17 +31,23 @@ export const mainAttr = "atk,cpct,cdmg"
 
 export const buffs = [
   {
-    title: "爱可菲天赋：当队伍中存在4名水/冰元素角色时，爱可菲的元素战技或元素爆发命中敌人后，将使该敌人的水元素抗性与冰元素抗性降低[kx]%",
+    title: "天赋-灵感浸入调味：队伍中存在4名水元素或冰元素元素角色时，敌方的冰元素抗性降低[kx]%",
     data: {
       kx: 55
     }
   }, {
-    title: "爱可菲1命：队伍中所有角色的元素类型均为水/冰元素时，爱可菲施放元素战技或元素爆发后，冰元素伤害的暴击伤害提升[cdmg]%",
+    title: "爱可菲1命：队伍中所有角色的元素类型均为水/冰元素时，暴击伤害提升[cdmg]%",
     cons: 1,
     data: {
       cdmg: 60
     }
+  }, {
+    title: "爱可菲4命：康复食疗效果的治疗量提升100%",
+    cons: 4,
+    data: {
+      heal: ({ params }) => params.kfsl ? 100 : 0
+    }
   }
 ]
 
-export const createdBy = "冰翼"
+export const createdBy = "其实雨很好"
