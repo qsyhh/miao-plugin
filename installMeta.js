f5328f6ccc0b4128cca12966dcc1a03b800f6563
其实雨很好 <2122840028@qq.com>
2025-08-27 22:27:48 +0800

    🐛fix: 存在攻略资源目录导致无法安装meta

--- installMeta.js
+++ installMeta.js
@@ -5,17 +5,24 @@ let installed = false
 
 async function installMeta() {
   let metaPath = "./plugins/miao-plugin/resources/meta-"
-  if (!fs.existsSync(`${metaPath}gs`) || !fs.existsSync(`${metaPath}sr`)) {
-    for (let game of [ "gs", "sr" ]) {
-      let command = `git clone -b meta-${game} https://cnb.cool/qsyhh_res/meta.git "${metaPath}${game}" --depth=1`
-      let execPro = async function execPro(cmd, ds = {}) {
-        return new Promise((resolve, reject) => {
-          exec(cmd, ds, (error, stdout, stderr) => {
-            resolve({ error, stdout, stderr })
-          })
+  if (!fs.existsSync(`${metaPath}gs/character`) || !fs.existsSync(`${metaPath}sr/character`)) {
+    let execPro = async function execPro(cmd, ds = {}) {
+      return new Promise((resolve, reject) => {
+        exec(cmd, ds, (error, stdout, stderr) => {
+          resolve({ error, stdout, stderr })
         })
+      })
+    }
+    for (let game of [ "gs", "sr" ]) {
+      let command, ret
+      let path = `${metaPath}${game}`
+      if (fs.existsSync(path)) {
+        command = `rm -r ${path}`
+        ret = await execPro(command)
+        if (ret.error) throw new Error(`删除meta-${game}目录失败！\nError code:  ` + ret.error.code + "\n" + ret.error.stack + "\n 请稍后重试。")
       }
-      let ret = await execPro(command)
+      command = `git clone -b meta-${game} https://cnb.cool/qsyhh_res/meta.git "${path}" --depth=1`
+      ret = await execPro(command)
       if (ret.error) {
         throw new Error(`meta-${game}资源包安装失败！\nError code:  ` + ret.error.code + "\n" + ret.error.stack + "\n 请稍后重试。")
       } else {
